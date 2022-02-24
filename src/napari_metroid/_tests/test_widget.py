6edc986e3995e4043578c5e61163c831aface42a
Marcelo Zoccoler <marcelozoc@gmail.com>
2022-02-24 18:27:07 +0100

    update tests and requirements

--- src/napari_metroid/_tests/test_widget.py
+++ src/napari_metroid/_tests/test_widget.py
@@ -1,4 +1,4 @@
-from napari_metroid import ExampleQWidget, example_magic_widget
+from napari_metroid import MainInterface
 import numpy as np
 
 # make_napari_viewer is a pytest fixture that returns a napari viewer object
@@ -9,7 +9,7 @@ def test_example_q_widget(make_napari_viewer, capsys):
     viewer.add_image(np.random.random((100, 100)))
 
     # create our widget, passing in the viewer
-    my_widget = ExampleQWidget(viewer)
+    my_widget = MainInterface(viewer)
 
     # call our widget method
     my_widget._on_click()
@@ -17,17 +17,3 @@ def test_example_q_widget(make_napari_viewer, capsys):
     # read captured output and check that it's as we expected
     captured = capsys.readouterr()
     assert captured.out == "napari has 1 layers\n"
-    
-def test_example_magic_widget(make_napari_viewer, capsys):
-    viewer = make_napari_viewer()
-    layer = viewer.add_image(np.random.random((100, 100)))
-
-    # this time, our widget will be a MagicFactory or FunctionGui instance
-    my_widget = example_magic_widget()
-
-    # if we "call" this object, it'll execute our function
-    my_widget(viewer.layers[0])
-
-    # read captured output and check that it's as we expected
-    captured = capsys.readouterr()
-    assert captured.out == f"you have selected {layer}\n"
